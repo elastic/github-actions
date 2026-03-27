acd967b681591aaf3d55251080facb98d9f06882
Brad White <brad.white@elastic.co>
2026-03-27 12:36:51 +1000

    fix formatting

--- litellm-token/src/index.ts
+++ litellm-token/src/index.ts
@@ -34,7 +34,9 @@ export async function run() {
 function isDirectExecution(): boolean {
   const scriptPath = process.argv[1];
 
-  return scriptPath !== undefined && typeof __filename !== 'undefined' && path.resolve(scriptPath) === __filename;
+  return (
+    scriptPath !== undefined && typeof __filename !== 'undefined' && path.resolve(scriptPath) === __filename
+  );
 }
 
 if (isDirectExecution()) {
